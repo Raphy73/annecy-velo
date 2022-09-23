4dca9065bdea37dfd0a99d7fadc41beb2d7ad23a
Jérémy Laurent <contact@jeremy-laurent.com>
2022-09-23 11:29:48 +0200

    feat(include-html): Allows user to include html file directly into another html file

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Replaces "include-html" tag with the content of the file
+
+    let all, i, el, file, xhttp;
+
+    all = document.getElementsByTagName("*");
+    for (i = 0; i < all.length; i++) {
+        el = all[i];
+        file = el.getAttribute("include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4) {
+                    if (this.status === 200) {el.innerHTML = this.responseText;}
+                    if (this.status === 404) {el.innerHTML = "Page not found.";}
+                    /* Remove the attribute, and call this function once more: */
+                    el.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+});
